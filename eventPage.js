13065945a1175295aaaa2587c4115f4e935f6cc6
Matt Downs <mattdowns6@gmail.com>
2015-11-01 17:02:15 +1000

    Links can now be hidden. Extension now reads from chrome synchronised storage, this paves the way for custom links!

--- eventPage.js
+++ eventPage.js
@@ -1,17 +1,84 @@
-// Google Analytics code placed in event page to reduce lag when opening extension popup.
+// Store links in memory for fast retrieval
+var linksBuffer = [];
 
+
+
+// Fetch the links from Chrome Storage
+function getLinks(callback) {
+  // Buffer is filled
+  if (linksBuffer.length > 0) return callback(linksBuffer);
+  // Get links from Chrome storage and fill buffer
+  chrome.storage.sync.get(['links', 'settings'], function(result) {
+    if (result.links && result.settings) {
+      console.log(result);
+      linksBuffer = result.links;
+    } else {
+      // No links found in Chrome storage, add defaults
+      chrome.storage.sync.set({
+        'links': defaultLinks,
+        'settings': true
+      });
+      linksBuffer = defaultLinks;
+    }
+    return callback(linksBuffer);
+  });
+}
+
+function updateChromeStorage(links) {
+  chrome.storage.sync.set({
+    'links': links
+  });
+}
+
+function popupOpened() {
+  // Send notification to google analytics
+  _gaq.push(['_trackPageview']);
+}
+
+var sendButtonAnalytics = function(label) {
+  _gaq.push(['_trackEvent', label, 'clicked']);
+};
+
+// Default data for first time setup
+var defaultSettings = {
+  allowAnalytics: true
+};
+var defaultLinks = [{
+  "name": "QUT Blackboard",
+  "URL": "http://blackboard.qut.edu.au/",
+  "protected": true,
+  "hidden": false
+}, {
+  "name": "QUT Virtual",
+  "URL": "https://qutvirtual.qut.edu.au/",
+  "protected": true,
+  "hidden": false
+}, {
+  "name": "Library",
+  "URL": "http://www.library.qut.edu.au/",
+  "protected": true,
+  "hidden": false
+}, {
+  "name": "cite|write",
+  "URL": "http://www.citewrite.qut.edu.au/",
+  "protected": true,
+  "hidden": false
+}, {
+  "name": "Assignment Minder",
+  "URL": "http://www.am.qut.edu.au/",
+  "protected": true,
+  "hidden": false
+}, {
+  "name": "Email",
+  "URL": "http://www.qut.edu.au/email",
+  "protected": true,
+  "hidden": false
+}];
+
+// Setup Google Analytics
 var analyticsID = 'UA-64329205-3';
-
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', analyticsID]);
-
-var popupOpened = function() {
-  _gaq.push(['_trackPageview']);
-};
-
-var buttonAnalytics = function(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-};
 
 (function() {
   var ga = document.createElement('script');
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-new Vue({
+var bgPage = chrome.extension.getBackgroundPage();
+var vm = new Vue({
   el: '#list',
 
   data: {
@@ -6,114 +7,62 @@ new Vue({
     links: []
   },
 
-  ready: function() {
-    this.fetchLinks();
+  created: function() {
+    this.getLinks();
+    this.popupOpened();
   },
 
   methods: {
-    fetchLinks: function() {
+    getLinks: function() {
+      // Expose Vue to pass into function
       var parentThis = this;
-      // Check if links exist in chrome storage
-      chrome.storage.sync.get('links', function(result) {
-        if (result.links) {
-          // Success! Push onto Vue model
-          parentThis.$set('links', result.links);
-        } else {
-          // Links not found in chrome storage, add defaults
-          var links = [{
-            name: 'QUT Blackboard',
-            URL: 'http://blackboard.qut.edu.au/',
-            protected: true,
-            hidden: false
-          }, {
-            name: 'QUT Virtual',
-            URL: 'https://qutvirtual.qut.edu.au/',
-            protected: true,
-            hidden: false
-          }, {
-            name: 'Library',
-            URL: 'http://www.library.qut.edu.au/',
-            protected: true,
-            hidden: false
-          }, {
-            name: 'cite|write',
-            URL: 'http://www.citewrite.qut.edu.au/',
-            protected: true,
-            hidden: false
-          }, {
-            name: 'Assignment Minder',
-            URL: 'http://www.am.qut.edu.au/',
-            protected: true,
-            hidden: false
-          }, {
-            name: 'Email',
-            URL: 'http://www.qut.edu.au/email',
-            protected: true,
-            hidden: false
-          }];
-
-          chrome.storage.sync.set({
-            links: links
-          });
-          // Push onto Vue model
-          parentThis.$set('links', links);
-        }
+      bgPage.getLinks(function(links) {
+        parentThis.links = links;
       });
+    },
+
+    popupOpened: function() {
+      bgPage.popupOpened();
     },
 
     openLink: function(link) {
       chrome.tabs.create({
         url: link.URL
       });
+      bgPage.sendButtonAnalytics(link.name);
     },
 
     toggleOrDeleteLink: function(link) {
       if (link.protected) {
         // Toggle hidden or visible for default links
         link.hidden = !link.hidden;
-        console.log(this.$get('links'));
       } else {
         // Delete personal links
       }
       // Save links in chrome storage
-      var links = this.$get('links');
-      chrome.storage.sync.set({
-        links: links
-      });
+      bgPage.updateChromeStorage(this.links);
     },
 
     showSettings: function() {
-      var settingsView = this.$get('settingsView');
-      if (settingsView) {
-        // Close settings view
-        $('.hide-or-delete-icon').animate({
-          paddingLeft: 30
-        }, 150);
-        $('#links-container').animate({
-          marginRight: -48
-        }, 150);
-        this.$set('settingsView', false);
-      } else {
+      if (!this.settingsView) {
         // Show settings view
+        this.settingsView = true;
         $('.hide-or-delete-icon').animate({
           paddingLeft: 10
         }, 150);
         $('#links-container').animate({
           marginRight: 0
         }, 150);
-        this.$set('settingsView', true);
+      } else {
+        // Close settings view
+        this.settingsView = false;
+        $('.hide-or-delete-icon').animate({
+          paddingLeft: 30
+        }, 150);
+        $('#links-container').animate({
+          marginRight: -48
+        }, 150);
       }
     }
   }
 });
-
-
-chrome.runtime.getBackgroundPage(function(eventPage) {
-  eventPage.popupOpened();
-});
-
-function buttonClicked(e) {
-  chrome.runtime.getBackgroundPage(function(eventPage) {
-    eventPage.buttonAnalytics(e);
-  });
-}
